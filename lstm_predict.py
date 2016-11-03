95813d64ee04130345ea850b150e7b7936844b06
shenglin1987 <zsl.zju@gmail.com>
2016-11-03 19:49:06 +0800

    Update lstm_predict.py

--- lstm_predict.py
+++ lstm_predict.py
@@ -7,8 +7,33 @@ from keras.layers.recurrent import GRU,LSTM
 from keras.datasets.data_utils import get_file
 import numpy as np
 import re
-import operator#
+#import operator
 #import helper
 from collections import Counter
 import sys
 from keras.models import model_from_json
+
+
+model = Sequential()
+model.add(Embedding(max_features, 128, input_length=maxlen, dropout=0.5))
+model.add(LSTM(128, dropout_W=0.5, dropout_U=0.5))  # try using a GRU instead, for fun
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+# try using different optimizers and different optimizer configs
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              class_mode="binary")
+
+wordModel = Sequential()
+wordModel.add(Embedding(vocSize, 256, input_length=maxWord))
+wordModel.add(Dropout(0.2))
+wordModel.add(LSTM(512, return_sequences=True))    
+wordModel.add(Dropout(.2))
+wordModel.add(LSTM(512, return_sequences=False))
+    #wordModel.add(RepeatVector(maxlen))
+    
+wordModel.add(Dense(vocSize))
+wordModel.add(Activation('softmax'))
+wordModel.compile(loss='categorical_crossentropy', optimizer='rmsprop')
