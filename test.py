4dedebb6b302893a1815fb8a9f531a5eb6772508
shenglin1987 <henry@HenryZhaos-MacBook-Air.local>
2016-11-18 15:09:59 +0800

    add facebook competition code

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
 #this is for test
+import pandas as pd
+import sys
+import numpy as np
+
+
+def transform(ifile, ofile):
+    '''
+    transform the data format to fit pandas
+    '''
+    head = 'uid,x,y,time,place_id\n'
+    ifile = open(ifile, 'r')
+    ofile = open(ofile, 'w')
+    ofile.write(head)
+    while True:
+        line = ifile.readline()
+        if not line:
+            break
+        if len(line) > 10:
+            ofile.write(line)
+
+    ifile.close()
+    ofile.close()
+
+if __name__ == '__main__':
+    transform(sys.argv[1], sys.argv[2])
